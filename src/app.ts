fd60efaa5fef85e5961e178ad57b618c0a1832a3
n1la9jan <nilanjanmridha89@gmail.com>
2026-01-24 03:17:54 +0530

    Refinement

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
 // src/App.ts
 // Define the Elysia app here
 
+import { Elysia } from 'elysia'
+import { cors } from '@elysiajs/cors'
+import { getAuth } from './shared/services/auth';
+import { authMiddleware } from './shared/middleware/auth';
 import { userRoutes } from './modules/user/user.routes';
 import { workspaceRoutes } from './modules/workspace/workspace.routes';
 import { habitRoutes } from './modules/habit/habit.routes';
 import { taskRoutes } from './modules/task/task.routes';
-import { swagger } from '@elysiajs/swagger';
-import { serverTiming } from '@elysiajs/server-timing';
-import { Elysia } from 'elysia'
-import { cors } from '@elysiajs/cors'
-import { authMiddleware } from './shared/middleware/auth';
-import { getAuth } from './shared/services/auth';
 import { analyticsRoutes } from './modules/analytics/analytics.routes';
 
 export function createApp() {
@@ -18,17 +16,11 @@ export function createApp() {
   const auth = getAuth();
 
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
     credentials: true,
-  }));
-
-  app.options("*", ({ set }) => {
-    set.status = 204;
-  });
-
-  app.all("/api/auth/*", ({ request }) => auth.handler(request.clone()));
-
-  app.derive(authMiddleware);
+  }))
+    .all("/api/auth/*", ({ request }) => auth.handler(request.clone()))
+    .derive(authMiddleware);
 
 
   userRoutes(app);
