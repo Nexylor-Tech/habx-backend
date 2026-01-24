2561444a4f1c8581021f609b92b876f3a283aedf
n1la9jan <nilanjanmridha89@gmail.com>
2026-01-24 16:26:27 +0530

    Let's see if this works

--- src/shared/middleware/auth.ts
+++ src/shared/middleware/auth.ts
@@ -5,7 +5,6 @@ import { getAuth } from "../services/auth";
 
 export const authMiddleware = async ({ request }: any) => {
   const auth = getAuth();
-  // const headers = Object.fromEntries(request.headers.entries());
   const session = await auth.api.getSession({ headers: request.headers });
   return { u: session?.user || null, session: session?.session || null };
 };
